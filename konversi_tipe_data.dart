1cede494f8b143f05c25505c3afcf109f8651ef1
cindymargarethac <cindymargarethac@gmail.com>
2023-10-09 11:01:03 +0800

    first commit

---
+++ konversi_tipe_data.dart
@@ -0,0 +1,18 @@
+void main() {
+  var inputString = '1000';
+  var inputInt = int.parse(inputString);
+  var inputDouble = double.parse(inputString);
+
+  var doubleFromInt = inputInt.toDouble();
+  var intFromDouble = inputDouble.toInt();
+
+  var stringFromInt = inputInt.toString();
+  var stringFromDouble = inputDouble.toString();
+
+/**Konversi Boolean dan String */
+
+var inputBool = inputString == 'true';
+
+var stringFromBool = inputBool.toString();
+
+}
